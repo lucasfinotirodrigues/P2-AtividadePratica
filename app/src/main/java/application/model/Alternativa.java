efc3401eef32a232af754223adcf8fcba54ecedd
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-11-21 17:55:31 +0000

    feat: files of model added

---
+++ app/src/main/java/application/model/Alternativa.java
@@ -0,0 +1,59 @@
+package application.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alternativas")
+public class Alternativa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String texto;
+
+    private boolean correta;
+
+    @ManyToOne
+    @JoinColumn(name = "questao_id")
+    private Questao questao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean isCorreta() {
+        return correta;
+    }
+
+    public void setCorreta(boolean correta) {
+        this.correta = correta;
+    }
+
+    public Questao getQuestao() {
+        return questao;
+    }
+
+    public void setQuestao(Questao questao) {
+        this.questao = questao;
+    }  
+}
